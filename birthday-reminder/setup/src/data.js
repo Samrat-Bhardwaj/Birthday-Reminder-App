c94cd14353a6d72dff4097947b46f483450ef260
SAM Bhardwaj <bsamrat47@gmail.com>
2021-01-21 18:42:08 +0530

    end

--- birthday-reminder/setup/src/data.js
+++ birthday-reminder/setup/src/data.js
@@ -1,29 +1,29 @@
 export default [
   {
     id: 1,
-    name: 'Bertie Yates',
-    age: 29,
+    name: 'Samrat Bhardwaj',
+    age: 21,
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      'https://images.immediate.co.uk/production/volatile/sites/3/2018/05/IRON-2008-d7a2706.jpg?quality=90&resize=768,574',
   },
   {
     id: 2,
-    name: 'Hester Hogan',
-    age: 32,
+    name: 'Rahul kore',
+    age: 21,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
   {
     id: 3,
-    name: 'Larry Little',
-    age: 36,
+    name: 'Shubham',
+    age: 20,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
   {
     id: 4,
-    name: 'Sean Walsh',
-    age: 34,
+    name: 'Arun Teltia',
+    age: 21,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
